84c6290b73dfd4c5cdb410f8631288a340d868bf
Sebastian Rodriguez <sebastianr213@gmail.com>
2015-12-12 17:37:41 -0300

    project controller to load basic project file

--- edat/controller/ProjectController.py
+++ edat/controller/ProjectController.py
@@ -28,12 +28,30 @@ class ProjectController:
     def _validate_new_project_config(name, path_location):
         validation_errors = None
         if os.path.isdir(path_location):
-            if os.path.isfile(os.path.join(path_location, name)):
+            if os.path.isfile(os.path.join(path_location, name + EDAT_PROJECT_EXTENSION)):
                 validation_errors = 'An existent file already has the same name.'
         else:
             validation_errors = 'The location selected is not a directory.'
         return validation_errors
 
-    def _create_project_file(self, name, path_location):
+    @staticmethod
+    def _create_project_file(name, path_location):
         with open(os.path.join(path_location, name + EDAT_PROJECT_EXTENSION), 'w+') as project_file:
-            project_file.write('creation_timestamp: {}\n'.format(datetime.now()))
+            project_file.write('creation_timestamp||{}\n'.format(datetime.now()))
+
+    @staticmethod
+    def load_project(name, path_location):
+        project_file_location = os.path.join(path_location, name + EDAT_PROJECT_EXTENSION)
+        if os.path.isdir(path_location) and os.path.isfile(project_file_location):
+            project = Project(name, path_location)
+            project.parse_file(project_file_location)
+            return project
+        else:
+            raise InfoException('There is no edat project on the selected location with that name')
+
+
+if __name__ == "__main__":
+    pc = ProjectController()
+    #pc.create_project('project_test', '/home/srodriguez/repos')
+    p = pc.load_project('project_test', '/home/srodriguez/repos')
+    print p
--- edat/model/Project.py
+++ edat/model/Project.py
@@ -5,6 +5,15 @@ class Project:
     def __init__(self, name, path_location):
         self.name = name
         self.path_location = path_location
+        self.creation_timestamp = None
 
     def __str__(self):
-        return "Project: {0} ({1})".format(self.name, self.path_location)
+        return "Project: {0} ({1}) [{2}]".format(
+            self.name,
+            self.path_location,
+            self.creation_timestamp)
+
+    def parse_file(self, project_file_location):
+        with open(project_file_location) as project_file:
+            content = project_file.readlines()
+            self.creation_timestamp = content[0].split('||')[1].strip()
