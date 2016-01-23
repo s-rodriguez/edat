13bf9a75194e2204e51f53d34b98456c6856395b
Sebastian Rodriguez <sebastianr213@gmail.com>
2016-01-23 17:05:48 -0300

    run transformer always if in debug

--- edat/__init__.py
+++ edat/__init__.py
@@ -16,13 +16,18 @@ def get_edat_directory():
     return os.path.dirname(os.path.realpath(__file__))
 
 
+def debug_actions():
+    from edat.utils.ui import transform_ui_files_into_py
+    transform_ui_files_into_py()
+
+
 def main(cls=None, method=None, resource=None):
+    if STATUS == 'debug':
+        debug_actions()
+
     app = QApplication(sys.argv)
     form = MainUiController()
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
-    if STATUS == 'debug':
-        from edat.utils.ui import transform_ui_files_into_py
-        transform_ui_files_into_py()
     main()
