6cba217fead11558d45d7c1ec9b81c546689668e
Sebastian Rodriguez <sebastianr213@gmail.com>
2016-04-11 21:11:26 -0300

    persist attribute panel config in data config

--- edat/ui/AttributeConfigurationView.py
+++ edat/ui/AttributeConfigurationView.py
@@ -9,18 +9,19 @@ from edat.utils.ui.TextUtils import TextUtils
 from af.model.algorithms.AfManager import AfManager
 from af.model.Attribute import Attribute
 
+
 class AttributeConfigurationView(QtGui.QFrame):
 
     def __init__(self, project_controller):
         super(QFrame, self).__init__()
         self.project_controller = project_controller
-        self.attributes = []
+        self.data_config = self.project_controller.project.data_config
 
         data_factory = DataFactory()
         af_manager = AfManager()
 
-        self.db_controller = data_factory.create_controller(self.project_controller.project.data_config.location,
-                                                            self.project_controller.project.data_config.type)
+        self.db_controller = data_factory.create_controller(self.data_config.location,
+                                                            self.data_config.type)
 
         main_layout = QtGui.QVBoxLayout()
 
@@ -62,30 +63,36 @@ class AttributeConfigurationView(QtGui.QFrame):
 
         self.setFrameStyle(QFrame.StyledPanel | QFrame.Plain)
 
+        self.attributes_combo.setCurrentIndex(0)
+        self.refresh_attribute(self.attributes_combo.currentText())
+
         main_layout.addLayout(attr_layout)
         self.setLayout(main_layout)
 
     def load_attributes(self):
-        attributes = self.db_controller.table_columns_info(self.project_controller.project.data_config.table)
+        if len(self.data_config.attributes_list) == 0:
+            attributes = self.db_controller.table_columns_info(self.project_controller.project.data_config.table)
+            for att_name in attributes:
+                self.data_config.attributes_list.append(Attribute(name=att_name))
+        else:
+            attributes = [att.name for att in self.data_config.attributes_list]
+
         self.attributes_combo.addItems(attributes)
         self.attributes_combo.currentIndexChanged['QString'].connect(self.refresh_attribute)
-        for att_name in attributes:
-            self.attributes.append(Attribute(name=att_name))
 
     def refresh_attribute(self, attribute_name):
         selected_attribute = str(self.attributes_combo.currentText())
 
-        for att in self.attributes:
+        for att in self.data_config.attributes_list:
             if att.name == selected_attribute:
-                for combo in (self.type_combo, self.category_combo, self.weight_spin_box):
-                    combo.blockSignals(True)
+                combos = (self.type_combo, self.category_combo, self.weight_spin_box)
+                self.block_objects_signals(combos, True)
 
                 self.type_combo.setCurrentIndex(self.type_combo.findText(att.basic_type))
                 self.category_combo.setCurrentIndex(self.category_combo.findText(att.privacy_type))
                 self.weight_spin_box.setValue(att.weight)
 
-                for combo in (self.type_combo, self.category_combo, self.weight_spin_box):
-                    combo.blockSignals(False)
+                self.block_objects_signals(combos, False)
                 break
 
     def save_attribute_info(self, text):
@@ -94,9 +101,14 @@ class AttributeConfigurationView(QtGui.QFrame):
         privacy_category = str(self.category_combo.currentText())
         weight = self.weight_spin_box.value()
 
-        for att in self.attributes:
+        for att in self.data_config.attributes_list:
             if att.name == selected_attribute:
                 att.basic_type = basic_type
                 att.privacy_type = privacy_category
                 att.weight = weight
                 break
+
+    @staticmethod
+    def block_objects_signals(objects_list, block=True):
+        for ob in objects_list:
+            ob.blockSignals(block)
