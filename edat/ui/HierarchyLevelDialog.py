7249fce380b68e594aaa0a49c170be3ceeab9221
Gustavo Silva de Sousa <gustavosilvadesousa@gmail.com>
2016-04-17 20:21:18 -0300

    hierarchy view improvements. add hiearchy level dialog for new levels

---
+++ edat/ui/HierarchyLevelDialog.py
@@ -0,0 +1,59 @@
+from PyQt4 import QtGui
+
+from PyQt4.QtCore import Qt
+
+
+class HierarchyLevelDialog(QtGui.QDialog):
+
+    def __init__(self, parent=None):
+        super(QtGui.QDialog, self).__init__(parent)
+
+        main_layout = QtGui.QVBoxLayout()
+
+        level_name_form_view = QtGui.QFormLayout()
+        self.level_name_edit_text = QtGui.QLineEdit()
+        level_name_form_view.addRow("Level Name: ", self.level_name_edit_text)
+        main_layout.addLayout(level_name_form_view)
+
+        self.level_items_view = QtGui.QListWidget()
+        main_layout.addWidget(self.level_items_view)
+
+        add_item_layout = QtGui.QHBoxLayout()
+
+        self.item_name = QtGui.QLineEdit()
+        add_item_layout.addWidget(self.item_name)
+        add_node_button = QtGui.QPushButton("Add")
+        add_node_button.clicked.connect(self.add_node)
+        add_item_layout.addWidget(add_node_button)
+        main_layout.addLayout(add_item_layout)
+
+        ok_and_cancel_buttons = QtGui.QDialogButtonBox(
+            QtGui.QDialogButtonBox.Ok | QtGui.QDialogButtonBox.Cancel,
+            Qt.Horizontal, self)
+        ok_and_cancel_buttons.accepted.connect(self.accept)
+        ok_and_cancel_buttons.rejected.connect(self.reject)
+
+        main_layout.addWidget(ok_and_cancel_buttons)
+
+        self.setLayout(main_layout)
+        self.setWindowTitle("Create Hierarchy Level")
+
+        self.show()
+
+    def add_node(self):
+        item_name = self.item_name.text()
+        if item_name:
+            for i in range(self.level_items_view.count()):
+                if item_name == self.level_items_view.item(i).text():
+                    return
+            self.item_name.clear()
+            self.level_items_view.addItem(item_name)
+
+    def get_level_items(self):
+        items = []
+        for i in range(self.level_items_view.count()):
+            items.append(str(self.level_items_view.item(i).text()))
+        return items
+
+
+
--- edat/ui/HierarchyView.py
+++ edat/ui/HierarchyView.py
@@ -1,63 +1,87 @@
 from PyQt4 import QtGui
 
-from PyQt4.QtGui import QTableWidgetItem
+from PyQt4.QtCore import Qt
 
 from af.controller.data.SqliteController import SqliteController
+from edat.ui.HierarchyLevelDialog import HierarchyLevelDialog
 
 
-class HierarchyView(QtGui.QDialog):
+class HierarchyView(QtGui.QMainWindow):
 
     def __init__(self, project_controller, attribute, parent=None):
-        super(QtGui.QDialog, self).__init__(parent)
+        super(QtGui.QMainWindow, self).__init__(parent)
 
         self.project_controller = project_controller
         self.attribute = attribute
+        self.hierarchy_levels = []
         # TODO:  se crea el SqliteController en vez de usar la factory,
         # ya que el Csvcontroller no tiene implementado el get_distinct_qi_values
         # una vez implementado, usar la factory
         self.db_controller = SqliteController(self.project_controller.project.data_config.location)
 
-        attribute_values = []
+        self.leaf_values = []
         for value in self.db_controller.get_distinct_qi_values(self.project_controller.project.data_config.table, self.attribute.name):
-            attribute_values.append(str(value))
+            self.leaf_values.append(str(value))
 
-        layout = QtGui.QHBoxLayout()
-        self.setLayout(layout)
+        self.hierarchy_levels.append(self.leaf_values)
 
-        self.table_view = QtGui.QTableWidget()
-        self.table_view.setColumnCount(2)
-        self.table_view.setRowCount(len(attribute_values))
-        self.fill_values(attribute_values)
-        self.build_columns_headers()
+        self.mainLayout = QtGui.QVBoxLayout()
 
-        layout.addWidget(self.table_view)
+        self.ctr_frame = QtGui.QWidget(self)
+        self.ctr_frame.setLayout(self.mainLayout)
+        self.setCentralWidget(self.ctr_frame)
+
+        self.hierarchy_table_view = QtGui.QTableWidget()
+        self.update_table_view()
+
+        self.mainLayout.addWidget(self.hierarchy_table_view)
+
+        self.hierarchy_table_view.resizeColumnsToContents()
+        self.hierarchy_table_view.resizeRowsToContents()
+
+        self.add_level_button = QtGui.QPushButton("New level")
+        self.add_level_button.setMaximumSize(100, 60)
+        self.add_level_button.clicked.connect(self.on_new_level)
+
+        self.mainLayout.addWidget(self.add_level_button, 0, Qt.AlignCenter)
 
         self.setWindowTitle(self.attribute.name + ' Hierarchy')
-        self.table_view.resizeColumnsToContents()
-        self.table_view.resizeRowsToContents()
-
-        self.setMinimumSize(600, 600)
+        self.showMaximized()
 
         self.show()
 
-    def fill_values(self, attribute_values):
+    def update_table_view(self):
+        self.hierarchy_table_view.setColumnCount(len(self.hierarchy_levels))
+        self.hierarchy_table_view.setRowCount(len(self.leaf_values))
+        self.fill_values()
+        self.build_columns_headers()
 
-        for n_row in range(0, len(attribute_values)):
-            for n_col in range(0, self.table_view.columnCount()):
+    def fill_values(self):
+        for n_row in range(0, len(self.leaf_values)):
+            for n_col in range(0, len(self.hierarchy_levels)):
                 if n_col == 0:
-                    widget = QtGui.QLabel()
-                    self.table_view.setItem(n_row, n_col, QTableWidgetItem(attribute_values[n_row]))
+                    self.hierarchy_table_view.setCellWidget(n_row, n_col, QtGui.QLabel(self.leaf_values[n_row]))
                 else:
                     widget = QtGui.QComboBox()
-                self.table_view.setCellWidget(n_row, n_col, widget)
+                    widget.addItems(list(self.hierarchy_levels[n_col]))
+                    self.hierarchy_table_view.setCellWidget(n_row, n_col, widget)
 
     def build_columns_headers(self):
-        for n_col in range(0, self.table_view.columnCount()):
+        for n_col in range(0, len(self.hierarchy_levels)):
             if n_col == 0:
                 column_name = 'Leaf level'
             else:
                 column_name = 'Level ' + str(n_col)
-            self.table_view.setHorizontalHeaderItem(n_col, QtGui.QTableWidgetItem(column_name))
+            self.hierarchy_table_view.setHorizontalHeaderItem(n_col, QtGui.QTableWidgetItem(column_name))
+
+    def on_new_level(self):
+        self.new_level_dialog = HierarchyLevelDialog(self)
+        if self.new_level_dialog.exec_():
+            level_items = self.new_level_dialog.get_level_items()
+            if level_items:
+                self.hierarchy_levels.append(level_items)
+                self.update_table_view()
+
 
 
 
